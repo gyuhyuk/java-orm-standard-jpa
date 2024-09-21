4f28611af9d2a4398d1369276be42c835586aa7a
gyuhyuk <bruno3@naver.com>
2024-09-22 00:42:25 +0900

    section 4-2 : 영속성 컨텍스트 2

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -11,15 +11,9 @@ public class JpaMain {
         tx.begin();
 
         try {
-            // 비영속
-            Member member = new Member();
-            member.setId(1L);
-            member.setName("HelloJPA");
+            Member member = em.find(Member.class, 150L);
+            member.setName("ZZZZZ");
 
-            // 영속 상태 (엔티티 매니저 안에 있는 영속성 컨텍스트를 통해서 멤버가 관리된다는 뜻)
-            em.persist(member);
-
-            // 커밋찍는 시점에 쿼리가 날라간다.
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
